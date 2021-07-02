4676066f1be0b4d5da6330abe6393db17e07e50e
yoyoyachu <ryachna@gmail.com>
2021-07-01 23:50:28 -0400

    made separate folder for backend

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs')
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get('/index',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(3001,()=>{
+    console.log('on port 3001')
+})
+
