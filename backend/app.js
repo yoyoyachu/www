8978e7d8c3f0847e9b5ce0000dd25d7948162b3b
yoyoyachu <ryachna@gmail.com>
2021-08-03 23:56:53 -0400

    media queries for tablet & big screen view

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,11 @@ const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const arr = require('./public/static/project-details');
-console.log(arr);
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
-
 
 app.get('/index',(req,res)=>{
     res.render('index')
