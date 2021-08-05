569747e5626a02b6f464d7dd754761eaf169e665
yoyoyachu <ryachna@gmail.com>
2021-08-04 22:56:25 -0400

    refactored layouts with partials

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
 const arr = require('./public/static/project-details');
 
+app.engine('ejs', ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/index',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('index')
+});
+app.get('/aboutme',(req,res)=>{
+    res.render('aboutme', {arr})
 });
 app.get('/project',(req,res)=>{
     res.render('project', {arr})
