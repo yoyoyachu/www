7af155c861c21cda9c9495750607a53a00878e9b
yoyoyachu <ryachna@gmail.com>
2021-08-10 16:04:36 -0400

    cursor effect on index page

---
+++ public/jscript/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // typing animation
+    (function ($) {
+        $.fn.writeText = function (content) {
+            var contentArray = content.split(""),
+                current = 0,
+                elem = this;
+            setInterval(function () {
+                if (current < contentArray.length) {
+                    elem.text(elem.text() + contentArray[current++]);
+                }
+            }, 110);
+        };
+    })(jQuery);
+    // input text for typing animation
+    $("#city").writeText("Greater New York City Area");
+    $("#job").writeText("Web Developer")
+});
