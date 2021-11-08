1fe7fc47d5d720348ffe81ac7c3a5ca726b5faba
yoyoyachu <ryachna@gmail.com>
2021-11-08 15:49:40 -0500

    changed name on home page

--- public/static/edu-details.js
+++ public/static/edu-details.js
@@ -1,41 +1,102 @@
 let eduDetails = [
-    {year: 
-        2021, 
-        details:[
-        {title: 
-            'React Developer Bootcamp (in progress)',
-        courseProvider:
-            'Udemy',
-        certi: 
-            ''},
-        {title: 'Web Applications for Everybody' , courseProvider: 'University Of Michigan, Coursera' , certi: 'https://www.coursera.org/account/accomplishments/specialization/WH86CL5P6ME9'},
-        {title: 'JavaScript, jQuery, and JSON' , courseProvider: 'University Of Michigan, Coursera' , certi: '​​https://www.coursera.org/account/accomplishments/certificate/A2Q6WZUT7RUY'},
-    
-        {title: 'Building Database Applications in PHP' , courseProvider: 'University Of Michigan, Coursera' , certi: '​​https://www.coursera.org/account/accomplishments/certificate/N84SHLXRGJVB'},
-    
-        {title: 'Introduction to Structured Query Language (SQL)' , courseProvider: 'University Of Michigan, Coursera' , certi: 'https://www.coursera.org/account/accomplishments/verify/88VL47ZBSAPT'},
-    
-        {title: 'Building Web Applications in PHP' , courseProvider: 'University Of Michigan, Coursera' , certi: '​​https://www.coursera.org/account/accomplishments/certificate/TB4VSU2ETMAM'},
-    
-        {title: 'HTML, CSS, and Javascript for Web Developers' , courseProvider: 'Johns Hopkins University, Coursera' , certi: 'https://www.coursera.org/account/accomplishments/verify/V8UC6LXZHTJA'},
-    
-        {title: 'The Web Developer Bootcamp' , courseProvider: 'Udemy' , certi: 'https://www.udemy.com/certificate/UC-6a101cd1-a7d5-4def-a3a1-c29e66b84b08/'}
-    ]},
-    {year: 2020, details:[
-        {title: 'The Complete Javascript Course 2020: Build Real Projects!' , courseProvider: 'Udemy' , certi: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-2beb45b8-ff42-4456-b9ab-90090d59abf3.pdf'},
+    {
+        year:
+            2021,
+        details: [
+            {
+                title: 'React Developer Bootcamp (in progress)',
+                courseProvider: 'Udemy',
+                certi: ''
+            },
+            {
+                title: 'Web Applications for Everybody',
+                courseProvider: 'University Of Michigan, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/specialization/WH86CL5P6ME9'
+            },
 
-        {title: 'Build Responsive Real World Websites With HTML5 and CSS3' , courseProvider: 'Udemy' , certi: '​​https://www.udemy.com/certificate/UC-f81586e9-c1ba-4279-b856-8d593b4ef0b2/'}
-    ]}, 
-    {year:2019, details:[
-        {title: 'Complete Python Bootcamp: Go from zero to hero in python 3' , courseProvider: 'Udemy' , certi: 'https://www.udemy.com/certificate/UC-C3BJ9JPE/'},
+            {
+                title: 'JavaScript, jQuery, and JSON',
+                courseProvider: 'University Of Michigan, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/verify/A2Q6WZUT7RUY'
+            },
 
-        {title: 'Computer Science 101: The Theory Behind Programming' , courseProvider: 'Udemy' , certi: 'https://www.udemy.com/certificate/UC-YST3GMJF/'},
+            {
+                title: 'Building Database Applications in PHP',
+                courseProvider: 'University Of Michigan, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/verify/N84SHLXRGJVB'
+            },
 
-        {title: 'Electronics & Communication Engineering', courseProvider: 'Gujarat Technological University, India', certi: ''}
-    ]},
-    {year:2016, details:[
-        {title: 'Siemens Basics of Programmable Logic Controller Certificate' , courseProvider: 'Siemens Center of Excellence' , certi: ''}
-    ]},
+            {
+                title: 'Introduction to Structured Query Language (SQL)',
+                courseProvider: 'University Of Michigan, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/verify/88VL47ZBSAPT'
+            },
+
+            {
+                title: 'Building Web Applications in PHP',
+                courseProvider: 'University Of Michigan, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/verify/TB4VSU2ETMAM'
+            },
+
+            {
+                title: 'HTML, CSS, and Javascript for Web Developers',
+                courseProvider: 'Johns Hopkins University, Coursera',
+                certi: 'https://www.coursera.org/account/accomplishments/verify/V8UC6LXZHTJA'
+            },
+
+            {
+                title: 'The Web Developer Bootcamp',
+                courseProvider: 'Udemy',
+                certi: 'https://www.udemy.com/certificate/UC-6a101cd1-a7d5-4def-a3a1-c29e66b84b08/'
+            }
+        ]
+    },
+    {
+        year: 2020,
+        details: [
+            {
+                title: 'The Complete Javascript Course 2020: Build Real Projects!',
+                courseProvider: 'Udemy',
+                certi: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-2beb45b8-ff42-4456-b9ab-90090d59abf3.pdf'
+            },
+
+            {
+                title: 'Build Responsive Real World Websites With HTML5 and CSS3',
+                courseProvider: 'Udemy',
+                certi: 'https://www.udemy.com/certificate/UC-f81586e9-c1ba-4279-b856-8d593b4ef0b2/'
+            }
+        ]
+    },
+    {
+        year: 2019,
+        details: [
+            {
+                title: 'Complete Python Bootcamp: Go from zero to hero in python 3',
+                courseProvider: 'Udemy',
+                certi: 'https://www.udemy.com/certificate/UC-C3BJ9JPE/'
+            },
+            {
+                title: 'Computer Science 101: The Theory Behind Programming',
+                courseProvider: 'Udemy',
+                certi: 'https://www.udemy.com/certificate/UC-YST3GMJF/'
+            },
+            {
+                title: 'Electronics & Communication Engineering',
+                courseProvider: 'Gujarat Technological University, India',
+                certi: ''
+            }
+        ]
+    },
+    {
+        year: 2016,
+        details: [
+            {
+                title: 'Siemens Basics of Programmable Logic Controller Certificate',
+                courseProvider: 'Siemens Center of Excellence',
+                certi: ''
+            }
+        ]
+    },
     // {year:2014, details:[
     //     {title: 'Navsari High School' , courseProvider: 'Gujarat Secondary and Higher Secondary Education Board, India' , certi: ''}
     // ]}
